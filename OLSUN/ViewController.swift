6678057101818fec10f7800fc177245b1eeb35af
narmin-b <bgvnrm@icloud.com>
2025-03-19 21:26:29 +0400

    Initial Commit

---
+++ OLSUN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OLSUN
+//
+//  Created by Narmin Baghirova on 19.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OLSUNTests/OLSUNTests.swift
@@ -0,0 +1,17 @@
+//
+//  OLSUNTests.swift
+//  OLSUNTests
+//
+//  Created by Narmin Baghirova on 19.03.25.
+//
+
+import Testing
+@testable import OLSUN
+
+struct OLSUNTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
